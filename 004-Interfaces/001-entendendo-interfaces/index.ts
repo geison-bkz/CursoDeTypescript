8b4a92646ce3d384e51efd86a9249c6f9dedc636
geison-bkz <geison_bf@hotmail.com>
2023-12-22 21:46:16 -0300

    Interfaces

--- 004-Interfaces/001-entendendo-interfaces/index.ts
+++ 004-Interfaces/001-entendendo-interfaces/index.ts
@@ -3,4 +3,31 @@
     Eles também fornecem nomes explícitos para verificação de tipo.
 */
 
-(() => {})();
+(() => {
+    interface IPessoa {
+        nome: string;
+        idade: number;
+        readonly cpf: number;
+        enabled(): boolean;
+    }
+
+    const pessoa: IPessoa = {
+        nome: 'Geison',
+        idade: 35,
+        cpf: 11111111111,
+        enabled: () => {
+            return true;
+        },
+    };
+
+    class Geison implements IPessoa {
+        nome: string = 'Geison';
+        idade: number = 35;
+
+        readonly cpf: number = 11111111111;
+
+        enabled(): boolean {
+            return true;
+        }
+    }
+})();
